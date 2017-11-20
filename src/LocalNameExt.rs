b0bf7321013a9c056cb186678200a723d8f1c699
Raphael Cohn <raphael.cohn@stormmq.com>
2017-11-20 15:15:40 +0000

    Added convenience methods

--- src/LocalNameExt.rs
+++ src/LocalNameExt.rs
@@ -19,6 +19,22 @@ pub trait LocalNameExt: Sized
 	{
 		self.attribute("")
 	}
+	
+	/// Turns a LocalName into an empty UnattachedNode
+	#[inline(always)]
+	fn empty_node(self) -> UnattachedNode;
+	
+	/// Turns a LocalName into an UnattachedNode with attributes
+	#[inline(always)]
+	fn with_attributes(self, attributes: Vec<Attribute>) -> UnattachedNode;
+	
+	/// Turns a LocalName into an UnattachedNode with attributes and text
+	#[inline(always)]
+	fn with_attributes_and_text<S: Into<String>>(self, attributes: Vec<Attribute>, text: S) -> UnattachedNode;
+	
+	/// Turns a LocalName into an UnattachedNode with text
+	#[inline(always)]
+	fn with_text<S: Into<String>>(self, text: S) -> UnattachedNode;
 }
 
 impl LocalNameExt for LocalName
@@ -33,5 +49,29 @@ impl LocalNameExt for LocalName
 	fn attribute(self, value: &str) -> Attribute
 	{
 		Attribute::local(self, value)
+	}
+	
+	#[inline(always)]
+	fn empty_node(self) -> UnattachedNode
+	{
+		UnattachedNode::empty(self)
+	}
+	
+	#[inline(always)]
+	fn with_attributes(self, attributes: Vec<Attribute>) -> UnattachedNode
+	{
+		UnattachedNode::with_attributes(self, attributes)
+	}
+	
+	#[inline(always)]
+	fn with_attributes_and_text<S: Into<String>>(self, attributes: Vec<Attribute>, text: S) -> UnattachedNode
+	{
+		UnattachedNode::with_attributes_and_text(self, attributes, text)
+	}
+	
+	#[inline(always)]
+	fn with_text<S: Into<String>>(self, text: S) -> UnattachedNode
+	{
+		UnattachedNode::with_text(self, text)
 	}
 }
--- src/UnattachedNode.rs
+++ src/UnattachedNode.rs
@@ -40,7 +40,7 @@ impl UnattachedNode
 	
 	/// Represents an empty element with just attributes and text, such as <details open>hello</details>
 	#[inline(always)]
-	pub fn with_attributes_and_text(local_name: LocalName, attributes: Vec<Attribute>, text: String) -> Self
+	pub fn with_attributes_and_text<S: Into<String>>(local_name: LocalName, attributes: Vec<Attribute>, text: S) -> Self
 	{
 		Self
 		{
@@ -48,29 +48,127 @@ impl UnattachedNode
 			attributes,
 			children: vec!
 			[
-				Left(text),
+				Left(text.into()),
 			],
+		}
+	}
+	
+	/// Represents an empty element with just text, such as <code>hello</code>
+	#[inline(always)]
+	pub fn with_text<S: Into<String>>(local_name: LocalName, text: S) -> Self
+	{
+		Self
+		{
+			local_name,
+			attributes: vec![],
+			children: vec!
+			[
+				Left(text.into()),
+			],
+		}
+	}
+	
+	/// Add an id attribute.
+	#[inline(always)]
+	pub fn with_id_attribute(self, id: &str) -> Self
+	{
+		self.with_attribute(local_name!("id").attribute(id))
+	}
+	
+	/// Add a title attribute.
+	#[inline(always)]
+	pub fn with_title_attribute(self, title: &str) -> Self
+	{
+		self.with_attribute(local_name!("title").attribute(title))
+	}
+	
+	/// Add a class attribute.
+	#[inline(always)]
+	pub fn with_class_attribute(self, classes: &str) -> Self
+	{
+		self.with_attribute(local_name!("class").attribute(classes))
+	}
+	
+	/// Add a class attribute from classes
+	#[inline(always)]
+	pub fn with_class_attribute_from_classes<S: Deref<Target=str>>(self, classes: &[S]) -> Self
+	{
+		let mut class_string = String::new();
+		let mut after_first = false;
+		for class in classes.iter()
+		{
+			if after_first
+			{
+				class_string.push(' ');
+			}
+			else
+			{
+				after_first = true;
+			}
+			class_string.push_str(class.deref());
+		}
+		self.with_class_attribute(&class_string)
+	}
+	
+	/// Add classes irrespective of whether a class attribute exists or not.
+	#[inline(always)]
+	pub fn with_classes<S: Deref<Target=str>>(mut self, classes: &[S]) -> Self
+	{
+		for class in classes.iter()
+		{
+			self = self.with_class(class.deref())
+		}
+		self
+	}
+	
+	/// Add a class attribute if it does not exist, or appends to an existing class attribute if it does
+	#[inline(always)]
+	pub fn with_class(mut self, class: &str) -> Self
+	{
+		let class_local_name = local_name!("class");
+		let mut found = false;
+		for attribute in self.attributes.iter_mut()
+		{
+			if attribute.name.is_only_local(&class_local_name)
+			{
+				if attribute.value.len32() != 0
+				{
+					attribute.value.push_char(' ');
+				}
+				attribute.value.push_slice(class);
+				found = true;
+				break;
+			}
+		}
+		if found
+		{
+			self
+		}
+		else
+		{
+			self.with_class_attribute(class)
 		}
 	}
 	
 	/// Add an attribute.
 	#[inline(always)]
-	pub fn add_attribute(&mut self, attribute: Attribute)
+	pub fn with_attribute(mut self, attribute: Attribute) -> Self
 	{
 		self.attributes.push(attribute);
+		self
 	}
 	
 	/// Add a child element.
 	#[inline(always)]
-	pub fn add_child_element(&mut self, child_element: UnattachedNode)
+	pub fn with_child_element(mut self, child_element: UnattachedNode) -> Self
 	{
 		self.children.push(Right(child_element));
+		self
 	}
 	
 	/// Add a child text. If there is an existing child text element, appends its text to it.
-	/// Returns a mutable reference to child_text.
 	#[inline(always)]
-	pub fn add_child_text(&mut self, child_text: String) -> &mut String
+	pub fn with_child_text(mut self, child_text: String) -> Self
 	{
 		if self.children.is_empty()
 		{
@@ -93,7 +191,7 @@ impl UnattachedNode
 				self.children.push(Left(child_text));
 			}
 		}
-		self.children.last_mut().unwrap().as_mut().left().unwrap()
+		self
 	}
 	
 	/// Attach this node as a child of the document_node (ie as a root node)
